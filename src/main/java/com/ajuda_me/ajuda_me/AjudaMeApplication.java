1756cfdea77857d260bcd128cd2fae3c12d2fbd7
VÍCTOR EDUARDO DOS SANTOS <victor01.santos@catolicasc.edu.br>
2022-03-10 20:00:05 -0300

    first commit

---
+++ src/main/java/com/ajuda_me/ajuda_me/AjudaMeApplication.java
@@ -0,0 +1,13 @@
+package com.ajuda_me.ajuda_me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjudaMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AjudaMeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ajuda_me/ajuda_me/AjudaMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajuda_me.ajuda_me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjudaMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
